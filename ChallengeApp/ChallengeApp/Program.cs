6ea2988b9eea4327c2796e7610c0be6a8dff771f
Lukas Jacobsen <lukas.jacobsen@ocab.no>
2024-02-13 22:34:49 +0100

    Zadanie domowe Dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hei, Lukas Jacobsen!");
+﻿var name = "Ewa";
+var male = "Kobieta";
+var age = 30;
+
+if (age > 30 && male == "Kobieta")
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" &&  age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (male == "Mezczyzna" && age > 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+else
+{
+    Console.WriteLine("");
+}
