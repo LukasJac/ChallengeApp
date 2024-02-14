514527b88854b0d7b1adf7a6b2f67e3d891d914b
Lukas Jacobsen <lukas.jacobsen@ocab.no>
2024-02-14 20:18:06 +0100

    Poprawione zadanie domowe Dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -2,7 +2,7 @@
 var male = "Kobieta";
 var age = 30;
 
-if (age > 30 && male == "Kobieta")
+if (age < 30 && male == "Kobieta")
 {
     Console.WriteLine("Kobieta ponizej 30 lat");
 }
@@ -10,7 +10,7 @@ else if (name == "Ewa" &&  age == 30)
 {
     Console.WriteLine("Ewa, lat 30");
 }
-else if (male == "Mezczyzna" && age > 18)
+else if (male == "Mezczyzna" && age < 18)
 {
     Console.WriteLine("Niepelnoletni mezczyzna");
 }
